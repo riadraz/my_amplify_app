8dbbdd4082ace1f9960dacec46e17a7b1f23cfb6
riadraz <riadraz@gmail.com>
2025-04-23 18:14:27 +0600

    202504 first commit

--- App.tsx
+++ App.tsx
@@ -35,3 +35,41 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
+// import React from "react";
+// import { Button, View, StyleSheet } from "react-native";
+
+// import { Amplify } from "aws-amplify";
+// import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
+
+// import outputs from "./amplify_outputs.json";
+
+// Amplify.configure(outputs);
+
+// const SignOutButton = () => {
+//   const { signOut } = useAuthenticator();
+
+//   return (
+//     <View style={styles.signOutButton}>
+//       <Button title="Sign Out" onPress={signOut} />
+//     </View>
+//   );
+// };
+
+// const App = () => {
+//   return (
+//     <Authenticator.Provider>
+//       <Authenticator>
+//         <SignOutButton />
+//       </Authenticator>
+//     </Authenticator.Provider>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   signOutButton: {
+//     alignSelf: "flex-end",
+//   },
+// });
+
+// export default App;
